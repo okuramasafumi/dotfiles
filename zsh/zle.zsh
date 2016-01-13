06ebfa32406afd7f8273e59d6ddb35f0d992b81b
Okura Masafumi <masafumi.o1988@gmail.com>
2016-01-13 23:28:58 +0900

    Introduce zsh-autosuggestions

--- zsh/zle.zsh
+++ zsh/zle.zsh
@@ -3,3 +3,12 @@ select-word-style default
 zstyle ':zle:*' word-chars ' /=;@:{}[]()<>,|.'
 zstyle ':zle:*' word-style unspecified
 WORDCHARS='*?_-[]~&!#$%^(){}<>'
+
+# Enable autosuggestions automatically.
+zle-line-init() {
+  zle autosuggest-start
+}
+zle -N zle-line-init
+
+bindkey '^T' autosuggest-toggle
+export AUTOSUGGESTION_ACCEPT_RIGHT_ARROW=1
