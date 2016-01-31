13a2e7cafaef7aef50ed5ab78329445949ab8e71
Okura Masafumi <masafumi.o1988@gmail.com>
2016-01-31 16:16:02 +0900

    New zsh autosuggestion configuration

--- zsh/zle.zsh
+++ zsh/zle.zsh
@@ -4,11 +4,10 @@ zstyle ':zle:*' word-chars ' /=;@:{}[]()<>,|.'
 zstyle ':zle:*' word-style unspecified
 WORDCHARS='*?_-[]~&!#$%^(){}<>'
 
-# Enable autosuggestions automatically.
-zle-line-init() {
-  zle autosuggest-start
-}
-zle -N zle-line-init
+# Add history-substring-search-* widgets to list of widgets that clear the autosuggestion
+ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(history-substring-search-up history-substring-search-down)
 
-bindkey '^T' autosuggest-toggle
-export AUTOSUGGESTION_ACCEPT_RIGHT_ARROW=1
+# Remove *-line-or-history widgets from list of widgets that clear the autosuggestion to avoid conflict with history-substring-search-* widgets
+ZSH_AUTOSUGGEST_CLEAR_WIDGETS=("${(@)ZSH_AUTOSUGGEST_CLEAR_WIDGETS:#(up|down)-line-or-history}")
+
+autosuggest_start
