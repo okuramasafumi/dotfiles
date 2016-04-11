9510bc5d252be0ce7b5169e976b72fe550e1ebe0
Okura Masafumi <masafumi.o1988@gmail.com>
2016-04-11 23:12:30 +0900

    Bind ctrl-space to suggestion accept

--- zsh/zle.zsh
+++ zsh/zle.zsh
@@ -4,8 +4,4 @@ zstyle ':zle:*' word-chars ' /=;@:{}[]()<>,|.'
 zstyle ':zle:*' word-style unspecified
 WORDCHARS='*?_-[]~&!#$%^(){}<>'
 
-# Add history-substring-search-* widgets to list of widgets that clear the autosuggestion
-ZSH_AUTOSUGGEST_CLEAR_WIDGETS+=(history-substring-search-up history-substring-search-down)
-
-# Remove *-line-or-history widgets from list of widgets that clear the autosuggestion to avoid conflict with history-substring-search-* widgets
-ZSH_AUTOSUGGEST_CLEAR_WIDGETS=("${(@)ZSH_AUTOSUGGEST_CLEAR_WIDGETS:#(up|down)-line-or-history}")
+bindkey '^ ' autosuggest-accept
