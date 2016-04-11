ce7b43052ab0ee95cc9c37bd76f8c6b25f64bcd7
Okura Masafumi <masafumi.o1988@gmail.com>
2016-04-11 23:12:58 +0900

    Add $GOPATH/bin to path

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,9 +1,9 @@
+export GOPATH=~/.go
+
 export EDITOR=vim
 export DISABLE_AUTO_TITLE=true
-export PATH=~/bin:$PATH
+export PATH=~/bin:$PATH:$GOPATH/bin
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
 # liquidprompt
 export LP_ENABLE_BATT=false
-
-export GOPATH=~/.go
