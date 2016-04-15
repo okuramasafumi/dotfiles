6df99deae73c98764b9fe5bddd122e7d441eb7ac
Okura Masafumi <masafumi.o1988@gmail.com>
2016-04-15 18:22:59 +0900

    Export TERM

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -2,6 +2,7 @@ export GOPATH=~/.go
 
 export EDITOR=vim
 export DISABLE_AUTO_TITLE=true
+export TERM="xterm-256color"
 export PATH=~/bin:$PATH:$GOPATH/bin
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
