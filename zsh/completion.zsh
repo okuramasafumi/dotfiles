60816d083c49acc0dd71d03706c7951d85312ff7
Okura Masafumi <masafumi.o1988@gmail.com>
2017-03-10 18:53:54 +0900

    Add zsh completion for ssh hosts

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,6 +1,11 @@
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
 
+# Use ~/.ssh/config for host completions
+zstyle -s ':completion:*:hosts' hosts _ssh_config
+[[ -r ~/.ssh/config ]] && _ssh_config+=($(cat ~/.ssh/config | sed -ne 's/Host[=\t ]//p'))
+zstyle ':completion:*:hosts' hosts $_ssh_config
+
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 if [[ "${terminfo[kcbt]}" != "" ]]; then
