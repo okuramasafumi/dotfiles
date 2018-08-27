285f690efdb654d44007e89674656da665df2aa0
okuramasafumi <masafumi.o1988@gmail.com>
2018-08-27 14:53:02 +0900

    Auto suggest accept with syntax highlight

--- zsh/zle.zsh
+++ zsh/zle.zsh
@@ -4,4 +4,16 @@ zstyle ':zle:*' word-chars ' /=;@:{}[]()<>,|.'
 zstyle ':zle:*' word-style unspecified
 WORDCHARS='*?_-[]~&!#$%^(){}<>'
 
-bindkey '^ ' autosuggest-accept
+# Workaround to enable syntax highlighting after accepting suggestions.
+# See: https://github.com/zsh-users/zsh-autosuggestions/issues/158
+my-autosuggest-accept() {
+    zle autosuggest-accept
+    zle redisplay
+    zle redisplay
+}
+
+zle -N my-autosuggest-accept
+
+ZSH_AUTOSUGGEST_IGNORE_WIDGETS+=my-autosuggest-accept
+
+bindkey '^ ' my-autosuggest-accept
