32ca83d6830ab1a4a85fcebb8af47f1beab01016
OKURA Masafumi <masafumi.o1988@gmail.com>
2023-06-21 22:34:07 +0900

    Improve fzf-cdr function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,9 +1,11 @@
-# https://www.rasukarusan.com/entry/2018/08/14/083000
-alias cdd='fzf-cdr'
-function fzf-cdr() {
-  target_dir=`cdr -l | sed 's/^[^ ][^ ]*  *//' | fzf`
-  target_dir=`echo ${target_dir/\~/$HOME}`
-  if [ -n "$target_dir" ]; then
-    cd $target_dir
+# https://gist.github.com/siriusjack/0b0032f22c72ffc7e5ba217f80674ad2
+function fzf-cdr () {
+  local selected_dir=$(cdr -l | awk '{ print $2 }' | fzf --preview "bat --color=always '{}/README.md' 2> /dev/null" --preview-window '~3' --bind 'ctrl-/:change-preview-window(60%|70%||30%|40%|50%)')
+  if [ -n "$selected_dir" ]; then
+    BUFFER="cd ${selected_dir}"
+    zle accept-line
   fi
+  zle clear-screen
 }
+zle -N fzf-cdr
+bindkey "^Z" fzf-cdr
