b15af043b67e07ddad69e14a5246703955187c90
OKURA Masafumi <masafumi.o1988@gmail.com>
2023-07-28 19:46:46 +0900

    Use bufferline.nvim instead of barbar.nvim
    I don't understand some behaviors of barbar.nvim.
    bufferline.nvim seems more straightforward.

--- nvim/init.lua
+++ nvim/init.lua
@@ -390,21 +390,9 @@ require("lazy").setup({
     end,
   },
   {
-    'romgrk/barbar.nvim', -- tabline
-    dependencies = {
-      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
-      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
-    },
-    init = function()
-      vim.g.barbar_auto_setup = false
-    end,
-    opts = {
-      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-      -- animation = true,
-      -- insert_at_start = true,
-      -- …etc.
-    },
-    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons'
   },
   {
     "nvim-zh/colorful-winsep.nvim", -- Colorful window separator
