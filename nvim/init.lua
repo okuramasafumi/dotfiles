9b732be7ba06d55e2841b6f7661a593b0bc83a9d
OKURA Masafumi <masafumi.o1988@gmail.com>
2024-03-26 23:14:13 +0900

    Depend on nvim-nio, which is now required

--- nvim/init.lua
+++ nvim/init.lua
@@ -474,6 +474,7 @@ require("lazy").setup({
   {
     'nvim-neotest/neotest',
     dependencies = {
+      "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
       "antoinemadec/FixCursorHold.nvim",
