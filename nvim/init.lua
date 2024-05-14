7740d418abc7da5c8027b39dd9a22fc896493702
OKURA Masafumi <masafumi.o1988@gmail.com>
2024-05-14 17:26:41 +0900

    Run linter at `InsertLeave` for better experience

--- nvim/init.lua
+++ nvim/init.lua
@@ -822,7 +822,7 @@ require('lint').linters_by_ft = {
   typescriptreact = {"eslint"},
 }
 
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
   callback = function()
     require("lint").try_lint()
   end,
