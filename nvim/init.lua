b56ab456194f0a25940c422f31577fb8b3598d09
OKURA Masafumi <masafumi.o1988@gmail.com>
2024-06-02 23:53:51 +0900

    Simplify ruby_lsp configuration
    It seems to cause errors with nvim 0.10.0, and simplifying it
    seemed to resolved it.

--- nvim/init.lua
+++ nvim/init.lua
@@ -727,43 +727,7 @@ lspconfig.jsonls.setup {
 
 -- lspconfig.steep.setup {}
 
--- The config is from https://github.com/neovim/nvim-lspconfig/pull/2498
-lspconfig.ruby_lsp.setup {
-  on_attach = function(client, buffer)
-    -- in the case you have an existing `on_attach` function
-    -- with mappings you share with other lsp clients configs
-    pcall(on_attach, client, buffer)
-    local diagnostic_handler = function ()
-      local params = vim.lsp.util.make_text_document_params(buffer)
-      client.request(
-      'textDocument/diagnostic',
-      {textDocument = params},
-      function(err, result)
-        if err then
-          local err_msg = string.format("ruby-lsp - diagnostics error - %s", vim.inspect(err))
-          vim.lsp.log.error(err_msg)
-        end
-        if not result then return end
-        vim.lsp.diagnostic.on_publish_diagnostics(
-        nil,
-        vim.tbl_extend('keep', params, { diagnostics = result.items }),
-        { client_id = client.id }
-        )
-      end
-      )
-    end
-    diagnostic_handler() -- to request diagnostics when attaching the client to the buffer
-    local ruby_group = vim.api.nvim_create_augroup('ruby_lsp', {clear = false})
-    vim.api.nvim_create_autocmd(
-    {'BufEnter', 'BufWritePre', 'InsertLeave', 'TextChanged'},
-    {
-      buffer = buffer,
-      callback = diagnostic_handler,
-      group = ruby_group,
-    }
-    )
-  end
-}
+lspconfig.ruby_lsp.setup {}
 
 require'lspconfig'.jdtls.setup{}
 
