79582439b15c6cc10731290ce8fec1e207f4fb8f
OKURA Masafumi <masafumi.o1988@gmail.com>
2025-03-19 20:10:16 +0900

    Replace `avante.nvim` with `codecompanion.nvim`

--- nvim/init.lua
+++ nvim/init.lua
@@ -335,49 +335,33 @@ require("lazy").setup({
     end
   },
   {
-    -- Copied from its README, https://github.com/yetone/avante.nvim
-    "yetone/avante.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
-    opts = {
-      -- add any opts here
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+    "olimorris/codecompanion.nvim",
     dependencies = {
-      "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "hrsh7th/nvim-cmp",            -- autocompletion for avante commands and mentions
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",      -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      adapters = {
+        copilot = function()
+          return require("codecompanion.adapters").extend("copilot", {
+            schema = {
+              model = {
+                default = "claude-3.7-sonnet",
+              },
+              max_tokens = {
+                default = 65536,
+              },
             },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
+          })
+        end,
       },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
+      strategies = {
+        chat = {
+          adapter = "copilot",
         },
-        ft = { "markdown", "Avante" },
+        inline = {
+          adapter = "copilot",
+        },
       },
     },
   },
