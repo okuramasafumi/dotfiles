d91d3a2d6d9700d951f4157324bc1faec71f7d45
OKURA Masafumi <masafumi.o1988@gmail.com>
2025-06-16 23:55:05 +0900

    Configure picker for obsidian.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -811,6 +811,25 @@ require("lazy").setup({
         },
       },
 
+      picker = {
+        -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', 'mini.pick' or 'snacks.pick'.
+        name = "telescope.nvim",
+        -- Optional, configure key mappings for the picker. These are the defaults.
+        -- Not all pickers support all mappings.
+        note_mappings = {
+          -- Create a new note from your query.
+          new = "<C-x>",
+          -- Insert a link to the selected note.
+          insert_link = "<C-l>",
+        },
+        tag_mappings = {
+          -- Add tag(s) to current note.
+          tag_note = "<C-x>",
+          -- Insert a tag at the current location.
+          insert_tag = "<C-l>",
+        },
+      },
+
       -- see below for full list of options 👇
     },
   },
