47014ddfcc3503acf7148f1a97e02fd64ce33ad5
OKURA Masafumi <masafumi.o1988@gmail.com>
2025-08-18 22:53:00 +0900

    Use `master` branch of `telescope.nvim`
    v1 branch is not updated for over 2 years.
    While it's still recommended in README, it's better to use `master`.

--- nvim/init.lua
+++ nvim/init.lua
@@ -449,7 +449,7 @@ require("lazy").setup({
   },
   {
     'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
+    branch = 'master',
     dependencies = {
       'nvim-lua/plenary.nvim',
       'LukasPietzschmann/telescope-tabs',
