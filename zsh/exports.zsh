36e9e32c5f9b5376865f6848d1879ca0c7cd0533
OKURA Masafumi <masafumi.o1988@gmail.com>
2026-01-01 23:52:55 +0900

    Set RUBYOPTS to fix OpenSSL error
    This is a workaround, and should be removed in the future.
    Also we need to make sure that `~/.rubyopenssl_default_store.rb`
    exists.
    https://github.com/ruby/openssl/issues/949#issuecomment-3367944960

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -7,6 +7,8 @@ export PATH="$HOME/bin:$GOPATH/bin:/usr/local/opt/python/libexec/bin:$PATH"
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 export MANPAGER='nvim +Man! --clean' # Use nvim as a man pager
 
+export RUBYOPT="-r$HOME/.rubyopenssl_default_store.rb $RUBYOPT"
+
 # FZF
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
